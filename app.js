cf7d81490823e2ca3d641e4cb062db98119c9f48
Fr4nee <65635671+Fr4nee@users.noreply.github.com>
2021-08-12 18:06:17 -0300

    js

--- app.js
+++ app.js
@@ -1,29 +1,16 @@
 // NAVABAR
 const navbar = document.querySelector('.navbar');
-window.onscroll = () => {
-    if (window.scrollY > 800) {
-        navbar.classList.add('nav-active');
-    } else {
-        navbar.classList.remove('nav-active');
+
+    window.onscroll = () => {
+        if (window.scrollY > 800) {
+            navbar.classList.add('nav-active');
+        } else {
+            navbar.classList.remove('nav-active');
+        }
     }
-};
 
-// TRANSLATE
-document.getElementById("ingles").style.display="none";
-function traducir() {  
-    document.getElementById("ingles").style.display="block";
-    document.getElementById("español").style.display="none";
-}
+    function showModal() {
+        document.getElementById("pageContent").style.opacity = "0.5";
+    }
 
-function showModal() {
-    document.getElementById("pageContent").style.opacity = "0.5";
-}
-
-// MODAL
-// var myModal = document.getElementById('modal')
-// var myInput = document.getElementById('btn_modal')
-
-// myModal.addEventListener('shown.bs.modal', function () {
-//     myInput.focus()
-// });
 
---
+++ translate.js
@@ -0,0 +1,14 @@
+var en;
+var es;
+
+
+function TraducirAlIngles() {  
+    en = document.getElementById("ingles");
+    en.innerHTML = "puta";
+}
+
+function TraducirAlEspaniol() {  
+    en = document.getElementById("ingles");
+    en.innerHTML = "putaaaaaaaa";
+
+}
