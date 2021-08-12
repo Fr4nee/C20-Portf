79451ebd8ab5cf532c5174cdcaf4f97f32594af4
Fr4nee <65635671+Fr4nee@users.noreply.github.com>
2021-08-12 11:36:10 -0300

    act 12/8

--- Conexion.php
+++ Conexion.php
@@ -1,18 +1,25 @@
 <?php
 
-$conexion = mysqli_connect('c20-portf.c0rxhojittix.us-east-2.rds.amazonaws.com','admin','adminroot','cveinte') or die (mysqli_error($mysqli));
+$usuario = "admin";
+$password = "laconchadetumadre";
+$servidor = "c20-portf.c0rxhojittix.us-east-2.rds.amazonaws.com";
+$basededatos = "cveinte";
+
+$conexion = mysqli_connect($servidor, $usuario, $password, $basededatos) or die ("Error");
 
 Insertar($conexion);
 
 function Insertar($conexion){
+
     $nombre = $_POST['name'];
     $email = $_POST['email'];
     $telefono = $_POST['tel'];
     $comentario = $_POST['coment'];
 
     $consulta = "insert into `clientes` (nombre, email, telefono, comentario) VALUES ('$nombre','$email','$telefono','$comentario')";
+
     mysqli_query($conexion,$consulta);
     mysqli_close($conexion);
-}
+};
 
 ?>
