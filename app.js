ea23b59260da3a76318202d775fa9c8b2a17b12d
Fr4nee <65635671+Fr4nee@users.noreply.github.com>
2021-08-17 15:01:18 -0300

    efectos y empiezo mas secciones

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+let animado = document.querySelectorAll(".equipo");
+
+function mostrarScroll() {
+    let scrollTop = document.documentElement.scrollTop;
+
+    for (var i = 0; i < animado.length; i++) {
+        let alturaAnimado = animado[i].offsetTop;
+
+        if (alturaAnimado - 600 < scrollTop) {
+            animado[i].style.opacity = 1;
+            animado[i].classList.add("mostrarArriba");
+        }
+    }
+}
+window.addEventListener('scroll', mostrarScroll);
 // NAVABAR
 const navbar = document.querySelector('.navbar');
 
@@ -12,5 +27,7 @@ const navbar = document.querySelector('.navbar');
     function showModal() {
         document.getElementById("pageContent").style.opacity = "0.5";
     }
+
+
 
 
